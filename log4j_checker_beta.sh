515e420299b0e540c455034c64cdc087dde879d9
Cagri Bayram <caggri@outlook.com>
2021-12-16 23:22:14 +0300

    Remediation functionality added

--- log4j_checker_beta.sh
+++ log4j_checker_beta.sh
@@ -1,9 +1,13 @@
+
 #!/bin/bash
 
 # source https://github.com/rubo77/log4j_checker_beta
 
 # needs locate to be installed, be sure to be up-to-date with
 # sudo updatedb
+
+# Remediation functionality is developed by Commencis Cloud Team.
+# https://www.commencis.com/commencis-cloud-transformation/
 
 # regular expression, for which packages to scan for:
 PACKAGES='solr\|elastic\|log4j'
@@ -28,6 +32,7 @@ function ok() {
 
 function locate_log4j() {
   if [ "$(command -v locate)" ]; then
+    information "using locate, which could be using outdated data. besure to have called updatedb recently"
     locate log4j
   else
     find \
@@ -35,8 +40,45 @@ function locate_log4j() {
       -name "*log4j*" \
       2>&1 \
       | grep -v '^find:.* Permission denied$' \
-      | grep -v '^find:.* No such file or directory$'
+      | grep -v '^find:.* No such file or directory$'   
   fi
+}
+
+function remediation_process() {
+information "JAR files are collecting"
+find \
+  /var /etc /usr /opt /lib* \
+  -name "*log4j*.jar" \
+  -type f \
+  2>&1 \
+  | grep -v '^find:.* Permission denied$' \
+  | grep -v '^find:.* No such file or directory$'  \
+  | while read -r line ; do \
+    if [[ $line == *"-1."* ]]; then
+      if zip -qq -d $line */JMSAppender.class 2>&1 > /dev/null  ; then
+        ok "$line remediated"
+      else
+        ok "Nothing to remediate on $line"
+      fi 
+    elif [[ $line == *"log4j-core"* ]]; then
+        if  zip -qq -d $line org/apache/logging/log4j/core/lookup/JndiLookup.class 2>&1 > /dev/null ; then
+          ok "$line remediated"
+        else
+          ok "Nothing to remediate on $line"
+        fi
+    fi
+done
+}
+
+function find_class_files() {
+  find /var /etc /usr /opt /lib* -type f\
+   -name "*.jar" -o \
+   -name "*.ear" -o \
+   -name "*.war" -o \
+   -name "*.properties" -o \
+   -name "*.class" -o \
+   -name "*.journal" \
+  | xargs grep -i 'JMSAppend\|JndiLookup' 
 }
 
 function find_jar_files() {
@@ -54,26 +96,7 @@ if [ $USER != root ]; then
   warning "You have no root-rights. Not all files will be found."
 fi
 
-# Set this if you have a download for sha256 hashes
-download_file=""
-dir_temp_hashes=$(mktemp -d)
-file_temp_hashes="$dir_temp_hashes/vulnerable.hashes"
-ok_hashes=
-if [[ -n $download_file && $(command -v wget) ]]; then
-        wget  --max-redirect=0 --tries=2 --no-netrc -O "$file_temp_hashes.in" -- "$download_file"
-elif [[ -n $download_file && $(command -v curl) ]]; then
-        curl --globoff -f "$download_file" -o "$file_temp_hashes.in"
-fi
-if [[ $? = 0 && -s "$file_temp_hashes.in" ]]; then
-        cat "$file_temp_hashes.in" | cut -d" " -f1 | sort | uniq  > "$file_temp_hashes"
-        ok_hashes=1
-        information "Downloaded vulnerable hashes from ..."
-fi
-
 information "Looking for files containing log4j..."
-if [ "$(command -v locate)" ]; then
-  information "using locate, which could be using outdated data. besure to have called updatedb recently"
-fi
 OUTPUT="$(locate_log4j | grep -iv log4js | grep -v log4j_checker_beta)"
 if [ "$OUTPUT" ]; then
   warning "Maybe vulnerable, those files contain the name:"
@@ -116,34 +139,34 @@ else
 fi
 
 information "Analyzing JAR/WAR/EAR files..."
-if [ $ok_hashes ]; then
-  information "Also checking hashes"
-fi
 if [ "$(command -v unzip)" ]; then
   find_jar_files | while read -r jar_file; do
     unzip -l "$jar_file" 2> /dev/null \
       | grep -q -i "log4j" \
       && warning "$jar_file contains log4j files"
-    if [ $ok_hashes ]; then
-      dir_unzip=$(mktemp -d)
-      base_name=$(basename "$jar_file")
-      unzip -qq -DD "$jar_file" '*.class' -d "$dir_unzip" \
-        && find "$dir_unzip" -type f -not -name "*"$'\n'"*" -name '*.class' -exec sha256sum "{}" \; \
-        | cut -d" " -f1 | sort | uniq > "$dir_unzip/$base_name.hashes";
-      num_found=$(comm -12 "$file_temp_hashes" "$dir_unzip/$base_name.hashes" | wc -l)
-      if [[ -n $num_found && $num_found != 0 ]]; then
-        warning "$jar_file contains vulnerable binary classes"
-      else
-        ok "No .class files with known vulnerable hash found in $jar_file at first level."
-      fi
-      rm -rf -- "$dir_unzip"
-    fi
   done
 else
   information "Cannot look for log4j inside JAR/WAR/EAR files (unzip not found)"
 fi
-[ $ok_hashes ] && rm -rf -- "$dir_temp_hashes"
 
+information "Searching for  JMSAppend or JndiLookup in files..."
+OUTPUT="$(find_class_files)"
+if [ "$OUTPUT" ]; then
+  warning "Maybe vulnerable, those files contain the name:"
+  printf "$OUTPUT\n"
+else
+  ok  "JMSAppend or JndiLookup does not exists."
+fi
+
+information "Remediation Process..."
+OUTPUT="$(remediation_process)"
+if [ "$OUTPUT" ]; then
+  printf "$OUTPUT"
+else
+  ok  "No vulnarable file found."
+fi
+
+echo
 information "_________________________________________________"
 if [ "$JAVA" == "" ]; then
   warning "Some apps bundle the vulnerable library in their own compiled package, so 'java' might not be installed but one such apps could still be vulnerable."
@@ -151,3 +174,4 @@ fi
 echo
 warning "This whole script is not 100% proof you are not vulnerable, but a strong hint"
 echo
+
